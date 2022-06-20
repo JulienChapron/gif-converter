9d64512c5a661342b1b1c47979b44a5e9e7d8233
Julien Chapron <60253712+JulienChapron@users.noreply.github.com>
2022-06-20 10:59:29 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import React, { AppBar, Toolbar, Typography } from "@mui/material";
 function Header() {
   return (
     <AppBar color="transparent" position="static">
